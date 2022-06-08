dca0e76fc071514e22f0dcd6ad96be317e9d56d2
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-08 21:25:00 +0200

    added new atributte to Product component showDescription

--- src/Product/Product.js
+++ src/Product/Product.js
@@ -5,18 +5,23 @@ import defaultImgSrc from '../fakeProducts/product.png';
 
 export default function Product(props) {
   const customStyle = props.customStyle;
-  const id = props.product.id;
   let image = props.product.image;
   const title = props.product.title;
   const price = props.product.price;
+  const showDescription = props.showDescription;
 
   if (image === '') {
     image = defaultImgSrc;
   }
 
   const clickDescriptionHandler = () => {
-    props.clickDescriptionHandler(id);
+    props.clickDescriptionHandler();
   };
+
+  let description = '';
+  if (showDescription) {
+    description = props.product.description;
+  }
 
   return (
     <article className={classes.Product} style={customStyle}>
@@ -27,6 +32,9 @@ export default function Product(props) {
         clickHandler={clickDescriptionHandler}
         text='Description'
         customStyle={{ display: 'block', margin: '0 auto 5px auto' }} />
+      <div>
+        {description}
+      </div>
     </article>
   );
 }
