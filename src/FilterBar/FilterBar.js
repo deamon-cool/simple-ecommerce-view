924445cfab39ebdb1104a7593692cc9798762ded
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-08 11:34:10 +0200

    created FilterBar container

---
+++ src/FilterBar/FilterBar.js
@@ -0,0 +1,11 @@
+import classes from './FilterBar.module.css';
+
+export default function FilterBar(props) {
+  const customStyle = props.customStyle;
+
+  return (
+    <nav className={classes.FilterBar} style={customStyle}>
+      {children}
+    </nav>
+  );
+}
