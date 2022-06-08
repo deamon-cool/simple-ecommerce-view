7b8d055e46e98ad189acfdf31ebbdb8fb2d19177
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-08 13:02:53 +0200

    implemented FilterBar with all needed components, states and handlers

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,60 @@
+import { useState } from 'react';
+import Button from '../Button/Button';
+import FilterBar from '../FilterBar/FilterBar';
+import Input from '../Input/Input';
+import Select from '../Select/Select';
 import classes from './App.module.css';
 
 function App() {
+  const [searchedProduct, setSearchedProduct] = useState('');
+  const [maxPrice, setMaxPrice] = useState('');
+  const [popularitySorting, setPopularitySorting] = useState('');
+  const [priceSorting, setPriceSorting] = useState('');
+
+  const searchHandler = (val) => {
+    setSearchedProduct(val)
+  };
+
+  const changePriceHandler = (val) => {
+    setMaxPrice(val);
+    // validation number
+    //parse float maxPrice to search products
+  };
+
+  const popularityClickHandler = () => {
+    //setPopularitySorting
+  };
+
+  const priceClickHandler = () => {
+    //setPriceSorting
+  };
+
+  const selectHandler = (val) => {
+    console.log(val)
+  };
+
   return (
     <div className={classes.App}>
-      
+      <FilterBar>
+        <Input
+          changeHandler={searchHandler}
+          value={searchedProduct}
+          placeholder='Search...' />
+        <Input
+          changeHandler={changePriceHandler}
+          value={maxPrice}
+          placeholder='Max price' />
+        <Button
+          clickHandler={popularityClickHandler}
+          text='Popularity' />
+        <Button
+          clickHandler={priceClickHandler}
+          text='Price' />
+        <Select
+          changeHandler={selectHandler}
+          optionsArr={['aaa', 'bbb', 'ccc']}
+          placeholder='Select category' />
+      </FilterBar>
     </div>
   );
 }
