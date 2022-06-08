f88a79f32e859e390e79eea6ef15ebe5f66269ab
DESKTOP-D7QJEB7\Damian <damian.damian.tofik@gmail.com>
2022-06-08 10:18:11 +0200

    added more attributes to Input

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,7 +1,19 @@
 import classes from './Input.module.css';
 
 export default function Input(props) {
+  const value = props.value;
+  const placeholder = props.placeholder;
+  const customStyle = props.customStyle;
+
+  function changeHanlder(e) {
+    console.log(e.target.value);
+    props.changeHandler(e.target.value);
+  }
+
   return (
-    <input className={classes.Input} />
+    <input className={classes.Input} style={customStyle}
+      onChange={changeHanlder}
+      value={value} 
+      placeholder={placeholder}/>
   );
 }
